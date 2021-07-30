fa11f1556c60daa2211976df3f899b426c1b6440
pilyha <illia.popov57@gmail.com>
2021-07-30 22:25:52 +0300

    Add application to personal finance management

--- module_3_nix_5/src/main/java/ua/com/nix/Main.java
+++ module_3_nix_5/src/main/java/ua/com/nix/Main.java
@@ -4,6 +4,7 @@ import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
 import ua.com.nix.model.User;
 import ua.com.nix.service.OperationControlService;
+import ua.com.nix.util.Connector;
 import ua.com.nix.util.HibernateConnector;
 
 import javax.persistence.EntityManager;
@@ -12,7 +13,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Main.main");
         Solution solution = new Solution();
-
+        Connector.createConnection(args[1], args[2]);
 
         try (SessionFactory sessionFactory = HibernateConnector.createSessionFactory(args[1], args[2])) {
             EntityManager entityManager = sessionFactory.createEntityManager();
