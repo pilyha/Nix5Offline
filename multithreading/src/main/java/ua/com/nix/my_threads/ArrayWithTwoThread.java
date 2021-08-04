68ab3225a5b9e9d651102a653deaaa5bbcb57403
pilyha <illia.popov57@gmail.com>
2021-08-04 23:46:45 +0300

    Add some solution_work by threads

---
+++ multithreading/src/main/java/ua/com/nix/my_threads/ArrayWithTwoThread.java
@@ -0,0 +1,57 @@
+package ua.com.nix.my_threads;
+
+import java.util.List;
+
+public class ArrayWithTwoThread implements Runnable{
+    private volatile List<Integer> list;
+    private Integer counter;
+    private Integer firstIndex;
+    private Integer lastIndex;
+
+
+    public ArrayWithTwoThread(List<Integer> list,Integer firstIndex, Integer lastIndex) {
+        this.list = list;
+        this.counter = 0;
+        this.firstIndex = firstIndex;
+        this.lastIndex = lastIndex;
+    }
+
+    @Override
+    public void run() {
+        for (int i = firstIndex; i < lastIndex; i++) {
+            if(checkSimple(i))
+            {
+                counter++;
+            }
+        }
+
+    }
+
+    private boolean checkSimple(int i) {
+        if (i <= 1)
+            return false;
+        else if (i <= 3)
+            return true;
+        else if (i % 2 == 0 || i % 3 == 0)
+            return false;
+        int n = 5;
+        while (n * n <= i) {
+            if (i % n == 0 || i % (n + 2) == 0)
+                return false;
+            n = n + 6;
+        }
+        return true;
+    }
+    public synchronized Object get()
+    {
+        while (counter == null) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return counter;
+    }
+}
---
+++ multithreading/src/main/java/ua/com/nix/my_threads/ReversePrinter.java
@@ -0,0 +1,12 @@
+package ua.com.nix.my_threads;
+
+public class ReversePrinter extends Thread{
+
+    public ReversePrinter(String name){
+        super(name);
+    }
+
+    @Override
+    public void run() {
+    }
+}
